50dd162d77d7de6e9e8700cab19ea5ef4ad944ea
JuanCalle1606 <jpmilojp@gmail.com>
2021-05-01 14:08:36 -0500

    Proyecto vacio creado

---
+++ src/Kys/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
