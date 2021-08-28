4f4b9819cd3de3826b2ac624e7f06569d5a48670
JuanCalle1606 <jpmilojp@gmail.com>
2021-08-28 18:59:53 -0500

    Agregadas mas funciones Standard

---
+++ src/Kys.Library/StandardFunctions/input.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable CS1591
+using System;
+
+namespace Kys.Library
+{
+	partial class StandardFunctions
+	{
+		[Function]
+		public static string input() => Console.ReadLine();
+	}
+}
---
+++ src/Kys.Library/StandardFunctions/key.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable CS1591
+using System;
+
+namespace Kys.Library
+{
+	partial class StandardFunctions
+	{
+		[Function]
+		public static ConsoleKeyInfo key() => Console.ReadKey();
+	}
+}
