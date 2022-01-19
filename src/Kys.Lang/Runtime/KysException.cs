28ceb2ff0553ba469cf24ef67cac209da9405d73
JuanCalle1606 <jpmilojp@gmail.com>
2022-01-19 10:02:14 -0500

    Agregada KysException

---
+++ src/Kys.Lang/Runtime/KysException.cs
@@ -0,0 +1,27 @@
+﻿using Antlr4.Runtime;
+
+namespace Kys.Lang.Runtime;
+
+/// <summary>
+/// Una excepción de kys.
+/// </summary>
+[Serializable]
+public class KysException : Exception
+{
+
+	protected KysException() : this(string.Empty, "Ha ocurrido un error en la ejecución del programa.") { }
+
+	public KysException(IToken exceptionToken, string msg) : base(msg)
+	{
+		Line = $"line {exceptionToken.Line}:{exceptionToken.Column}:";
+	}
+
+	protected KysException(string msg) : this(string.Empty, msg) { }
+
+	public KysException(string interval, string msg) : base(msg)
+	{
+		Line = interval;
+	}
+
+	public string Line { get; protected set; }
+}
